74dd7a0c99612593f6018bc3620c0b362cb15815
Anthonyme <anthonymh14@hotmail.com>
2023-04-20 16:58:58 -0400

    updated header
    basic header styling begin

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Card } from '@mui/material'
 
 
 const Header = () => {
   return (
-    <div>Im the Header and logo</div>
+    <Card sx={{
+    mt: 0,
+    pt: 0,
+    width: '100%',
+    backgroundColor: "blue",
+    color:'white',
+    textAlign: 'center'}} >
+
+
+  Im a Header
+  </Card>
   )
 }
 
